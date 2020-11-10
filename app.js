c4eedb53964ac9b442798dc35e182963791be3c1
kiran-code <ravikiransjce.code@gmail.com>
2020-11-10 07:28:47 +0530

    Coding for weather app by name, id, lat and long

---
+++ app.js
@@ -0,0 +1,33 @@
+
+const resumeBtn = document.getElementById("resumeBtn");
+const showWeatherBtn = document.getElementById("showWeatherBtn");
+const resumeSection = document.getElementById("resumeSection");
+const weatherSection = document.getElementById("weatherSection");
+
+const getWeatherBtn = document.getElementById('getWeatherBtn');
+
+document.addEventListener("load", function () {});
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  weatherSection.classList = "hideSection";
+  resumeBtn.classList.add('activeTabStyle');
+});
+
+resumeBtn.addEventListener("click", showResume);
+
+function showResume() {
+  resumeSection.classList = "showSection";
+  resumeBtn.classList.add('activeTabStyle');
+
+  weatherSection.classList = "hideSection";
+  showWeatherBtn.classList.remove('activeTabStyle');
+}
+
+showWeatherBtn.addEventListener("click", showWeather);
+
+function showWeather() {
+  resumeSection.classList = "hideSection";
+  resumeBtn.classList.remove('activeTabStyle');
+  weatherSection.classList = "showWSection";
+  showWeatherBtn.classList.add('activeTabStyle'); 
+}
---
+++ weather.js
@@ -0,0 +1,73 @@
+const apiKey = 'd9da5c116c793405f65774bb82a48990';
+const url = 'api.openweathermap.org/data/2.5/weather?lat=17.6936&lon=77.3210';
+
+function getWeatherDataByName(){
+  console.log('getWeatherDataByName');
+  const cityName = document.getElementById('cityNameInput').value;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName},IN&appid=d9da5c116c793405f65774bb82a48990`
+  fetchWeatherData(url, 'weatherByName');
+}
+
+function getWeatherDataById(){
+  console.log('getWeatherDataById');
+  const cityId = document.getElementById('cityIdInput').value;
+  let url = `https://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=d9da5c116c793405f65774bb82a48990`;
+  fetchWeatherData(url, 'weatherById');
+}
+
+function getWeatherDataByLatLong(){
+  console.log('getWeatherDataByLatLong');
+  const cityLatitude = document.getElementById('cityLatitude').value;
+  const cityLongitude = document.getElementById('cityLongitude').value;
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${cityLatitude}&lon=${cityLongitude}&appid=d9da5c116c793405f65774bb82a48990`
+  fetchWeatherData(url, 'weatherReportByLatLong');
+}
+
+function fetchWeatherData(url, generateReportBy){
+  fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    generateWeatherReport(data, generateReportBy);
+  })
+  .catch(err => {
+    showError(generateReportBy);
+  });
+}
+
+function showError(generateReportBy){  
+  const loc = document.getElementById(generateReportBy);
+  loc.innerHTML = '';
+
+  const errorP = document.createElement('p');
+  const errorMessage = 'Error Occurred,please try again';
+  errorP.innerHTML = errorMessage;
+  errorP.classList.add('error'); 
+  loc.appendChild(errorP);
+}
+
+function convertToCelsius(tempInKelvin){
+  return (+tempInKelvin - 272.15).toFixed(1);
+}
+
+function generateWeatherReport(weatherData, generateReportBy){
+  console.log('weather Data',weatherData, generateReportBy);
+
+  const loc = document.getElementById(generateReportBy);
+  loc.innerHTML = '';
+  const cityName = document.createElement('p');
+  cityName.innerHTML = `City: ${weatherData.name}`;
+  
+  const cityId = document.createElement('p');
+  cityId.innerHTML = `City: ${weatherData.id}`;
+  
+  const temperature = document.createElement('p');
+  temperature.innerHTML = `temperature: ${convertToCelsius(weatherData.main.temp)}`;
+  
+  const humidity = document.createElement('p');
+  humidity.innerHTML = `Humidity: ${weatherData.main.humidity}`;
+  
+  loc.appendChild(cityName);
+  loc.appendChild(cityId);
+  loc.appendChild(temperature);
+  loc.appendChild(humidity);
+}

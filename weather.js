317e2bbc1eb48366bff3093b0e4bbc1e203cba4d
kiran-code <ravikiransjce.code@gmail.com>
2020-11-10 08:08:32 +0530

    Improved styling, added reset logic

--- weather.js
+++ weather.js
@@ -8,11 +8,21 @@ function getWeatherDataByName(){
   fetchWeatherData(url, 'weatherByName');
 }
 
+function resetWeatherReportByName(){
+  document.getElementById('weatherByName').innerHTML = '';
+  document.getElementById('cityNameInput').value = '';
+}
+
 function getWeatherDataById(){
   console.log('getWeatherDataById');
   const cityId = document.getElementById('cityIdInput').value;
   let url = `https://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=d9da5c116c793405f65774bb82a48990`;
   fetchWeatherData(url, 'weatherById');
+}
+
+function resetWeatherReportById(){
+  document.getElementById('weatherById').innerHTML = '';
+  document.getElementById('cityIdInput').value = '';
 }
 
 function getWeatherDataByLatLong(){
@@ -21,6 +31,12 @@ function getWeatherDataByLatLong(){
   const cityLongitude = document.getElementById('cityLongitude').value;
   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${cityLatitude}&lon=${cityLongitude}&appid=d9da5c116c793405f65774bb82a48990`
   fetchWeatherData(url, 'weatherReportByLatLong');
+}
+
+function resetWeatherReportByLatLong(){
+  document.getElementById('weatherReportByLatLong').innerHTML = '';
+  document.getElementById('cityLatitude').value = ''
+  document.getElementById('cityLongitude').value = ''
 }
 
 function fetchWeatherData(url, generateReportBy){
@@ -49,6 +65,7 @@ function convertToCelsius(tempInKelvin){
   return (+tempInKelvin - 272.15).toFixed(1);
 }
 
+
 function generateWeatherReport(weatherData, generateReportBy){
   console.log('weather Data',weatherData, generateReportBy);
 
@@ -61,7 +78,7 @@ function generateWeatherReport(weatherData, generateReportBy){
   cityId.innerHTML = `City: ${weatherData.id}`;
   
   const temperature = document.createElement('p');
-  temperature.innerHTML = `temperature: ${convertToCelsius(weatherData.main.temp)}`;
+  temperature.innerHTML = `Temperature: ${convertToCelsius(weatherData.main.temp)}`;
   
   const humidity = document.createElement('p');
   humidity.innerHTML = `Humidity: ${weatherData.main.humidity}`;
